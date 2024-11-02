c56b402d1347432d9892a1c8698d3c5934d288f9
Ashish-Sunil-Vaidya <ashuvaidya2003@gmail.com>
2024-11-02 19:07:53 +0530

    Question page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+export default {
+  content: [],
   theme: {
     extend: {},
   },
